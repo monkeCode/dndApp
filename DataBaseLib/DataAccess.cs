35474f2565c81f581e9bc3fe98cd6b2b79189b71
Georgy <georgy.tertychniy@yandex.ru>
2021-07-04 01:00:13 +0300

    database auto loader

--- DataBaseLib/DataAccess.cs
+++ DataBaseLib/DataAccess.cs
@@ -2,30 +2,23 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using Windows.ApplicationModel;
 using Windows.Storage;
 
 namespace DataBaseLib
 {
     static public class DataAccess
     {
-
+        const string DB_NAME = "sqliteSample.db";
         public async static void InitializeDatabase()
         {
-            await ApplicationData.Current.LocalFolder.CreateFileAsync("sqliteSample.db", CreationCollisionOption.OpenIfExists);
-            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
-            using (SqliteConnection db =
-               new SqliteConnection($"Filename={dbpath}"))
+            //await ApplicationData.Current.LocalFolder.CreateFileAsync("sqliteSample.db", CreationCollisionOption.OpenIfExists);
+            if (await ApplicationData.Current.LocalFolder.TryGetItemAsync(DB_NAME) == null)
             {
-                db.Open();
-
-                string tableCommand = "CREATE TABLE IF NOT " +
-                    "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
-                    "Text_Entry NVARCHAR(2048) NULL)";
-
-                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
-
-                createTable.ExecuteReader();
+                StorageFile databaseFile = await Package.Current.InstalledLocation.GetFileAsync(/*$"Assets/*/DB_NAME/*"*/);
+                await databaseFile.CopyAsync(ApplicationData.Current.LocalFolder);
             }
+           
         }
 
         public static void AddData(string inputText)
@@ -57,6 +50,7 @@ namespace DataBaseLib
             //    entries[i] = new string[columns.Length];
 
             string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            
             using (SqliteConnection db =
                new SqliteConnection($"Filename={dbpath}"))
             {
